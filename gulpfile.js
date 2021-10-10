7335893c1267e32c134f6cf163533fa15c453dbc
anhpv27 <vietanhp@reactplus.com>
2021-10-10 10:47:23 +0700

    update: live server

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,13 @@
 
 //list dependences
 const { src, dest, watch, series } = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
-const prefix = require("gulp-autoprefixer");
-const minify = require("gulp-clean-css");
-const terser = require("gulp-terser");
-const imagemin = require("gulp-imagemin");
-const imagewebp = require("gulp-webp");
+var sass = require("gulp-sass")(require("sass")),
+  prefix = require("gulp-autoprefixer"),
+  minify = require("gulp-clean-css"),
+  terser = require("gulp-terser"),
+  imagemin = require("gulp-imagemin"),
+  imagewebp = require("gulp-webp"),
+  browserSync = require("browser-sync").create();
 
 //compile, prefix, and min scss
 function compilescss() {
@@ -48,13 +49,29 @@ function font() {
   return src("src/font/iciel/**/*.{ttf,otf}").pipe(dest("dist/assets/fonts"));
 }
 
+function browsersyncServe(cb) {
+  browserSync.init({
+    server: {
+      baseDir: ".",
+    },
+  });
+
+  cb();
+}
+
+function browsersyncReload(cb) {
+  browserSync.reload();
+  cb();
+}
+
 //watchtask
 function watchTask() {
-  watch("src/scss/**/*.scss", compilescss);
-  watch("src/js/*.js", jsmin);
-  watch("src/images/*", optimizeimg);
-  watch("dist/images/*.{jpg,png,jpeg}", webpImage);
-  watch("src/font/iciel/**/*.{ttf,otf}", font);
+  watch("src/scss/**/*.scss", series(compilescss, browsersyncReload));
+  watch("src/js/*.js", series(jsmin, browsersyncReload));
+  watch("src/images/*", series(optimizeimg, browsersyncReload));
+  watch("dist/images/*.{jpg,png,jpeg}", series(webpImage, browsersyncReload));
+  watch("src/font/iciel/**/*.{ttf,otf}", series(font, browsersyncReload));
+  watch("*.html", browsersyncReload);
 }
 
 // Default Gulp task
@@ -64,5 +81,6 @@ exports.default = series(
   optimizeimg,
   webpImage,
   font,
+  browsersyncServe,
   watchTask
 );
